962cbdaa400a9bb66fbc327ab45e3745a33f5ee2
worachai3 <worachai3@live.com>
2017-10-09 13:45:07 +0700

    added snake

--- snake.py
+++ snake.py
@@ -9,10 +9,18 @@ class SnakeWindow(arcade.Window):
 
         arcade.set_background_color(arcade.color.BLACK)
 
-    def main():
-        window = SnakeWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
-        arcade.set_window(window)
-        arcade.run()
+        self.snake_sprite = arcade.Sprite('images/block.png')
+        self.snake_sprite.set_position(300, 300)
+
+    def on_draw(self):
+        arcade.start_render()
+        
+        self.snake_sprite.draw()
+
+def main():
+    window = SnakeWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
+    arcade.set_window(window)
+    arcade.run()
 
 if __name__ == '__main__':
     main()
