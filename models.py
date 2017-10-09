8a1f75e02ab9cf11e54efeea66ac3a417aa19066
worachai3 <worachai3@live.com>
2017-10-09 19:22:56 +0700

    wait and move

--- models.py
+++ models.py
@@ -1,15 +1,23 @@
 import arcade
 
 class Snake:
+    MOVE_WAIT = 0.2
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
         self.y = y
 
+        self.wait_time = 0
+
     def update(self, delta):
+        self.wait_time += delta
+        if self.wait_time < Snake.MOVE_WAIT:
+            return
+
         if self.x > self.world.width:
             self.x = 0
         self.x += 5
+        self.wait_time = 0
 
 class World:
     def __init__(self, width, height):
